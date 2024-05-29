c62d84e12ee6354e58e453bbe12869a59462c8d3
viraj1607 <merai.viraj@gmail.com>
2024-05-28 21:53:51 -0400

    Sing in/Sign up

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Login from "../pages/Login";
+import Browse from "../pages/Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={appRouter} />
+    </div>
+  );
+};
+
+export default Body;
